5fb5cfd0fa37360c05d3b95d86a51850f8f71e06
lusamreth <somrethoverkill@gmail.com>
2023-11-28 20:28:38 +0700

    add medical record entity

---
+++ domains/MedicalRecordEntity/Intake_Class.java
@@ -0,0 +1,38 @@
+package Medical_Record.MedicalRecordEntity;
+
+public class Intake {
+    private String timeUse; // morning, afternoon,...
+    private int frequency;
+    private String remark;
+
+    public Intake(String timeUse, int frequency, String remark) {
+        this.timeUse = timeUse;
+        this.frequency = frequency;
+        this.remark = remark;
+    }
+
+    public String getTimeUse() {
+        return timeUse;
+    }
+
+    public void setTimeUse(String timeUse) {
+        this.timeUse = timeUse;
+    }
+
+    public int getFrequency() {
+        return frequency;
+    }
+
+    public void setFrequency(int frequency) {
+        this.frequency = frequency;
+    }
+
+    public String getRemark() {
+        return remark;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark;
+    }
+
+}
---
+++ domains/MedicalRecordEntity/Prescription.java
@@ -0,0 +1,58 @@
+package Medical_Record.MedicalRecordEntity;
+
+public class Prescription {
+    private String perID;
+    private String medName;
+    private String dosageUnit;
+    private int dosage;
+    private Intake intakeInstruction;
+
+    public Prescription(String perID, String medName, String dosageUnit, int dosage, Intake intakeInstruction) {
+        this.perID = perID;
+        this.medName = medName;
+        this.dosageUnit = dosageUnit;
+        this.dosage = dosage;
+        this.intakeInstruction = intakeInstruction;
+    }
+
+    public String getPerID() {
+        return perID;
+    }
+
+    public void setPerID(String perID) {
+        this.perID = perID;
+    }
+
+    public String getMedName() {
+        return medName;
+    }
+
+    public void setMedName(String medName) {
+        this.medName = medName;
+    }
+
+    public String getDosageUnit() {
+        return dosageUnit;
+    }
+
+    public void setDosageUnit(String dosageUnit) {
+        this.dosageUnit = dosageUnit;
+    }
+
+    public int getDosage() {
+        return dosage;
+    }
+
+    public void setDosage(int dosage) {
+        this.dosage = dosage;
+    }
+
+    public Intake getIntakeInstruction() {
+        return intakeInstruction;
+    }
+
+    public void setIntakeInstruction(Intake intakeInstruction) {
+        this.intakeInstruction = intakeInstruction;
+    }
+
+}
