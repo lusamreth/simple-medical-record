43c2e8403971c2fc1072ed673059dc825dc7d2ce
lusamreth <somrethoverkill@gmail.com>
2023-11-28 20:28:51 +0700

    add date entity

---
+++ domains/Date_Class.java
@@ -0,0 +1,59 @@
+package Medical_Record;
+
+import java.util.Calendar;
+import java.util.Date;
+
+public class Date {
+    private int day;
+    private int month;
+    private int year;
+    private static int age;
+
+    public Date(int day, int month, int year) {
+        this.day = day;
+        this.month = month;
+        this.year = year;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public void setDay(int day) {
+        this.day = day;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public static int calculateAge(Date birthDate, Date currentDate) {
+        Calendar birthCalendar = Calendar.getInstance();
+        birthCalendar.setTime(birthDate);
+
+        Calendar currentCalendar = Calendar.getInstance();
+        currentCalendar.setTime(currentDate);
+
+        age = currentCalendar.get(Calendar.YEAR) - birthCalendar.get(Calendar.YEAR);
+
+        // Adjust age if the birthdate hasn't occurred yet this year
+        if (currentCalendar.get(Calendar.DAY_OF_YEAR) < birthCalendar.get(Calendar.DAY_OF_YEAR)) {
+            age--;
+        }
+
+        return age;
+    }
+
+}
