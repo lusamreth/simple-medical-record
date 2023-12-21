e1628e76bd0d7adfefb5fb78d43dc1e86d39ec8c
lusamreth <somrethoverkill@gmail.com>
2023-12-21 13:37:05 +0700

    add medicalRecordEntity

---
+++ app/src/main/java/simple/medical/record/domains/MedicalRecordEntity/MedicalRecord.java
@@ -0,0 +1,89 @@
+package simple.medical.record.domains.MedicalRecordEntity;
+
+import java.util.Date;
+// import simple.medical.record.domains.Patient;
+
+import simple.medical.record.domains.Address;
+import simple.medical.record.domains.Patient;
+import simple.medical.record.domains.Doctor;
+// import domains.Date;
+// import domains.Doctor;
+// import domains.Patient;
+
+public class MedicalRecord {
+    private String medID;
+    private Date dateofVisit;
+    private Prescription prescription;
+    private Diagnosis diagnosis;
+    private Patient patientID;
+    private Doctor docID;
+    private String description;
+
+    public MedicalRecord(String medID, Date dateofVisit, Prescription prescription,
+            Diagnosis diagnosis, Patient patientID, Doctor docID, String description) {
+        this.medID = medID;
+        this.dateofVisit = dateofVisit;
+        this.prescription = prescription;
+        this.diagnosis = diagnosis;
+        this.patientID = patientID;
+        this.docID = docID;
+        this.description = description;
+    }
+
+    public String getMedID() {
+        return medID;
+    }
+
+    public void setMedID(String medID) {
+        this.medID = medID;
+    }
+
+    public Date getDateofVisit() {
+        return dateofVisit;
+    }
+
+    public void setDateofVisit(Date dateofVisit) {
+        this.dateofVisit = dateofVisit;
+    }
+
+    public Prescription getPrescription() {
+        return prescription;
+    }
+
+    public void setPrescription(Prescription prescription) {
+        this.prescription = prescription;
+    }
+
+    public Diagnosis getDiagnosis() {
+        return diagnosis;
+    }
+
+    public void setDiagnosis(Diagnosis diagnosis) {
+        this.diagnosis = diagnosis;
+    }
+
+    public Patient getPatientID() {
+        return patientID;
+    }
+
+    public void setPatientID(Patient patientID) {
+        this.patientID = patientID;
+    }
+
+    public Doctor getDocID() {
+        return docID;
+    }
+
+    public void setDocID(Doctor docID) {
+        this.docID = docID;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+}
