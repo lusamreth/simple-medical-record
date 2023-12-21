0230c6974661524de400beaa717b18fce60dc586
lusamreth <somrethoverkill@gmail.com>
2023-12-21 13:37:40 +0700

    add hospital entity

---
+++ app/src/main/java/simple/medical/record/domains/Hospital.java
@@ -0,0 +1,29 @@
+// package domains;
+package simple.medical.record.domains;
+
+public class Hospital {
+    private String name;
+    private Address address;
+
+    public Hospital(String name, Address address) {
+        this.name = name;
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+}
